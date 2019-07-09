3871c2abe29174a0e78b9f64e217a61085d724a6
mathisdomage <creeplands67@gmail.com>
2019-07-09 17:36:00 +0200

    ajout test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+board = [[1], [1], [1], [1], [], [], []]
+position = board[1][1]
+def display():
+    print("╔═══╦═══╦═══╦═══╦═══╦═══╦═══╗")
+    for c in range (5, -1, -1):
+        print("║", end ="")
+        for r in range (7):
+            try:
+                if board[r][c] == 1:
+                    piece = "O"
+                else:
+                    piece = "X"
+                print("", piece, "║", end = "")
+            except IndexError as e:
+                print("  ", "║", end = "")     
+        print("")
+        if c > 0 :
+            print("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
+        else:
+            print("╚═══╩═══╩═══╩═══╩═══╩═══╩═══╝")
+    print("  1   2   3   4   5   6   7")
+
+def winning_move(board,position):
+    if p
+
