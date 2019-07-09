2566e99a6c8e2910bd44cea5a3796dc7575d2d58
mathisdomage <creeplands67@gmail.com>
2019-07-09 11:29:34 +0200

    Instauration du tableau

--- Connect.py
+++ Connect.py
@@ -1,4 +1,18 @@
 board= [[1],[],[],[],[],[],[]]
-def display(board):
-    print("1","2","3","4","5","6","7")
-    
+row= 6
+col= 7
+
+def display():
+    print(" 1"," 2"," 3"," 4"," 5"," 6"," 7")
+    print("╔══╦══╦══╦══╦══╦══╦══╗")
+    diff = 5
+    for c in range (row):
+        print("║",end="")
+        for r in range (col):
+            try:
+                print(board[r][c+diff],"║",end ="")
+            except Exception as e:
+                print(" ","║",end="")     
+        print("")
+        diff= diff-2
+display()
