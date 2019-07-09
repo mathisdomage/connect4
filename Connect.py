e4e75bdb5c1ead3a9a47d0dd9d23ed462e2f1ebf
mathisdomage <creeplands67@gmail.com>
2019-07-09 14:49:08 +0200

    oublies d'espaces

--- Connect.py
+++ Connect.py
@@ -3,7 +3,7 @@ board= [[1],[2],[],[],[],[],[]]
 def display():
     print("╔═══╦═══╦═══╦═══╦═══╦═══╦═══╗")
     for c in range (5, -1, -1):
-        print("║", end="")
+        print("║", end = "")
         for r in range (7):
             try:
                 if board[r][c] == 1:
@@ -12,7 +12,7 @@ def display():
                     piece = "X"
                 print("", piece, "║", end = "")
             except IndexError as e:
-                print("  ", "║", end="")     
+                print("  ", "║", end = "")     
         print("")
         if c > 0 :
             print("╠═══╬═══╬═══╬═══╬═══╬═══╬═══╣")
