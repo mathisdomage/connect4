da8b016f4409a7c49a9dfdae32a7fcefe95be3a2
mathisdomage <creeplands67@gmail.com>
2019-07-09 17:35:17 +0200

    fichier tdt et fonctionement 2j de base

--- Connect.py
+++ Connect.py
@@ -1,9 +1,11 @@
-board= [[1],[2],[],[],[],[],[]]
-
+import os
+board = [[], [], [], [], [], [], []]
+player =1
 def display():
+    os.system("clear")
     print("╔═══╦═══╦═══╦═══╦═══╦═══╦═══╗")
     for c in range (5, -1, -1):
-        print("║", end = "")
+        print("║", end ="")
         for r in range (7):
             try:
                 if board[r][c] == 1:
@@ -19,4 +21,30 @@ def display():
         else:
             print("╚═══╩═══╩═══╩═══╩═══╩═══╩═══╝")
     print("  1   2   3   4   5   6   7")    
+
+def play():
+    col = int(input("Which column do you want to play ?"))    
+    #Si cette ligne n'est pas là le joueur ne peut pas jouer la colonne 7 
+    board[col-1].append(player)
+    display()
+  
+
+def winning_move(board,col):
+    win = False
+
+
+
+
+
+
+
 display()
+while True:    
+    print("It's Player",player,"'s Turn")
+    play()
+    print(col)
+    print(boardheight)
+    if player == 1:
+        player = 2
+    else:
+        player = 1
